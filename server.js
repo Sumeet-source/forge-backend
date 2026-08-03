287b724da56df8e7631732ffb18f6cebce076063
Sumeet-source <dhakads458669@gmail.com>
2026-08-03 14:00:48 +0530

    Fixed CORS, 404 routes, and added IPv4 direct connection

--- server.js
+++ server.js
@@ -4,8 +4,25 @@ const cors = require('cors');
 
 const app = express();
 
+// Explicit CORS to allow your specific Vercel domain and localhost
+const allowedOrigins = [
+  'https://my-ui-project-six.vercel.app',
+  'http://localhost:5173',
+  'http://localhost:5174'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(cors());
 
 // --- ROUTES ---
 const authRoutes = require('./routes/authRoutes.js');
@@ -13,16 +30,14 @@ const productRoutes = require('./routes/productRoutes.js');
 const orderRoutes = require('./routes/orderRoutes.js');
 
 app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes); // <--- This route now gets loaded!
 app.use('/api/orders', orderRoutes);
 
-// --- DATABASE (DIRECT CONNECTION - 100% BYPASSES DNS BLOCK) ---
-// directConnection=true forces Mongoose to ignore SRV lookups completely
+// --- DATABASE (DIRECT CONNECTION - IPv4 FORCED) ---
 const MONGO_URI = 'mongodb://dhakad458669_db_user:f1xRo9VUjGwPtsu@cluster0-shard-00-00.alwcqf.mongodb.net:27017,cluster0-shard-00-01.alwcqf.mongodb.net:27017,cluster0-shard-00-02.alwcqf.mongodb.net:27017/forge_db?ssl=true&authSource=admin&retryWrites=true&w=majority&directConnection=true&connectTimeoutMS=10000';
 
 const connectDB = async () => {
     try {
-        // Force IPv4 and give it plenty of time to connect
         const options = {
             family: 4,
             serverSelectionTimeoutMS: 10000
