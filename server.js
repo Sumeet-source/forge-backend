d443d40bb10179f11c971390620863c339968367
Sumeet-source <dhakads458669@gmail.com>
2026-08-03 19:59:00 +0530

    Typed SRV string manually

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.options('*', cors({ origin: '*', credentials: true }));
 app.use(express.json());
 
 // --- DATABASE ONLY (NO ROUTES) ---
-const MONGO_URI = 'mongodb://dhakad458669_db_user:f1xRo9VUjGwPtsu@cluster0-shard-00-00.alwcqf.mongodb.net:27017,cluster0-shard-00-01.alwcqf.mongodb.net:27017,cluster0-shard-00-02.alwcqf.mongodb.net:27017/forge_db?ssl=true&authSource=admin&retryWrites=true&w=majority&connectTimeoutMS=30000';
+
+
+const MONGO_URI = 'mongodb+srv://dhakads458669_db_user:f1xRo9VUjGwPtsu@cluster0.alwcqf.mongodb.net/forge_db?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDB = async () => {
     try {
